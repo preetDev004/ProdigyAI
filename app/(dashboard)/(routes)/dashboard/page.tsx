675625a86cf1757f90e0e0388b796a935b12dc9b
preet patel <preetpatelofficial004@gmail.com>
2024-01-18 18:51:28 -0500

    clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const DashboardPage = () => {
   return (
+    <>
+    
     <div>DashboardPage (Protected)</div>
+    <UserButton afterSignOutUrl='/'/>
+    </>
   )
 }
 
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage (Unprotected)</div>
-  )
-}
+    <>
+      <div>LandingPage (Unprotected)</div>
+      <div>
+        <Link href="/sign-in">
+          <Button variant="secondary"> Log in</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button variant="secondary"> Sign up</Button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
